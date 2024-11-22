d1c10776529859009a8d60f29acca97c033663c3
sofigoerling <sgoerling@udesa.edu.ar>
2024-11-22 19:18:49 -0300

    arreglando Home y createPost

---
+++ my-project-name/src/components/Post.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { db } from '../firebase/config';
+
+export default class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comentarios: [], 
+        };
+    }
+
+    componentDidMount() {
+        
+        db.collection('posts')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot((docs) => {
+            let arrDocs = [];
+            docs.forEach((doc) => {
+                arrDocs.push({
+                    id: doc.id, 
+                    data: doc.data(), 
+                });
+            });
+
+     
+            this.setState({
+                comentarios: arrDocs,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Posts</Text>
+                <FlatList
+                    data={this.state.comentarios}
+                    keyExtractor={(item) => item.id.toString()} 
+                    renderItem={({ item }) => (
+                        <View style={styles.commentCard}>
+                            <Text style={styles.commentText}>
+                                <Text style={styles.userEmail}>{item.data.userEmail}: </Text>
+                               
+                            </Text>
+                            <Text>{item.data.texto}</Text>
+                            <Text style={styles.date}>
+                                {item.data.createdAt?.toDate?.().toLocaleString() || 'Sin fecha'}
+                            </Text>
+                        </View>
+                    )}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#f9f9f9',
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    commentCard: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 5,
+        shadowColor: 'pink',
+        shadowOpacity: 5,
+        shadowRadius: 5,
+        elevation: 2,
+    },
+    commentText: {
+        fontSize: 16,
+        color: '#333',
+    },
+    username: {
+        fontWeight: 'bold',
+        color: '#007bff',
+    },
+    date: {
+        marginTop: 5,
+        fontSize: 12,
+        color: '#666',
+        textAlign: 'right',
+    },
+});
+
+
+
+
--- my-project-name/src/screens/CreatePost.js
+++ my-project-name/src/screens/CreatePost.js
@@ -1,14 +1,116 @@
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { Component } from 'react'
-import PostForm from '../components/PostForm'
+import { Text, View, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { auth, db } from '../firebase/config';
 
 export default class CreatePost extends Component {
-    render() {
-        return (
-          <View>
-            <PostForm navigation={this.props.navigation}/>
-          </View>
-        )
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+        texto: '', 
+        error: '',
+        verificacion: "", //pare verificar si el posteo esta subido y luego mandar a home
+    };
 }
+
+enviarPosteo() {
+    const { texto } = this.state;
+
+    if (texto.length !== 0) {
+        // Aca debe ir que verifique que el usuario esté logueado HACER
+        const user = auth.currentUser;
+
+            const nuevoPost = {
+                texto: texto,
+                createdAt: new Date(), // Fecha de creación
+                userEmail: user.email, // Email del usuario creador
+                likes: [], 
+            };
+
+            // Guardar en Firestore
+            db.collection('posts')
+                .add(nuevoPost)
+                .then(() => {
+                    this.setState( {verificacion: 'El posteo fue creado exitosamente.'})
+                    this.setState({ texto: '' }); 
+                    
+                })
+                
+    } else {
+                this.setState( {error: "Ingrese un texto"})
+                return ;
+        }
+}
+
+irAHome(){
+    this.props.navigation.navigate('home')
+}
+
+render() {
+    return (
+        <View style={styles.container}>
+            <Text>Crear Nuevo Posteo</Text>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                    placeholder="Escribe tu posteo aquí..."
+                    onChangeText={(texto) => this.setState({ texto })}
+                    value={this.state.texto}
+                />
+
+                {
+                    this.state.error !== '' &&
+                    <Text style={styles.errorText}>
+                        {this.state.error}
+                    </Text>
+                }
+                {
+                    this.state.verificacion !== "" && 
+                    this.irAHome()
+                }
+                 
+                <TouchableOpacity
+                    onPress={() => this.enviarPosteo() }
+        
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Crear Posteo</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+}
+
+const styles = StyleSheet.create({
+container: {
+    padding: 20,
+},
+input: {
+    borderWidth: 2,
+    borderColor: '#ff69b4',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 4,
+},
+button: {
+    backgroundColor: '#fca3b7',
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    textAlign: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#ff69b4',
+    alignItems: 'center',
+},
+buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+},
+errorText: {
+    color: 'red',
+    marginVertical: 5,
+    textAlign: 'center',
+},
+});
 
